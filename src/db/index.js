4d79daa3ec7a1cf1b808a74b54a303d642050e5f
SourinChattaraj <sourinchattaraj99@gmail.com>
2025-05-21 12:07:47 +0530

    getting started

---
+++ src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants";
+
+const connectDB = async () => {
+    try {
+      const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+      console.log(`\n MongoDB connected : ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("Error connection to MongoDb : ", error);
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require('dotenv').config() there is no issue, but it's not recommended to use it in the code
+import dotenv from "dotenv";
+import connectDB from "./db";
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+
+
+
+
+
+/*
+import express from "express";
+const app = express();
+;(async()=>{
+    try {
+        mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("Error to connect to MongoDB ",error)
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    } catch (error) {
+        console.log("Error connecting to MongoDB ", error);
+        throw error;
+    }
+})()
+// first approch to connect db to mern  */ 
