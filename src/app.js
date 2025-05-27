83a8d91738590efd120b90ac3022f9a742082018
SourinChattaraj <sourinchattaraj99@gmail.com>
2025-05-27 11:04:02 +0530

    error message and response message set

--- src/app.js
+++ src/app.js
@@ -8,5 +8,7 @@ app.use(cors({
     credentials: true, // Allow cookies to be sent with requests
 }))
 app.use(express.json({limit:'16kb'})); // Parse JSON bodies with a limit of 16kb
-
+app.use(express.urlencoded({extended: true, limit:'16kb'})); // Parse URL-encoded bodies with a limit of 16kb
+app.use(express.static('public')); // Serve static files from the 'public' directory
+app.use(cookieParser()); // Parse cookies from the request headers
 export default app;
---
+++ src/utils/asynHandler.js
@@ -0,0 +1,23 @@
+// using promise to handle async errors in express
+const asyncHandler=(requestHandler)=>{
+    (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((error)=>next(error));
+    }
+}
+export default asyncHandler;
+
+// using try-catch
+
+// const asyncHandler=(fn)= async (req,res,next)=>{
+
+//     try{
+//         await fn(req,res,next);
+//     }catch(error){
+//         res.status(error.code || 500).json({
+//             sucess: false,
+//             message: error.message || 'Internal Server Error',
+//         })
+//     }
+
+// }
