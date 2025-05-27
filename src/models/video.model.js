4bf2ec125b41d82b77363c889a0f379de6722570
SourinChattaraj <sourinchattaraj99@gmail.com>
2025-05-27 17:51:01 +0530

    add user and video models with JWT and bcrypt integration

---
+++ src/models/video.model.js
@@ -0,0 +1,43 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const videoSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        videoUrl: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        thumbnail: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        duration: {
+            type: Number, // duration in seconds
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: true,
+        },
+        owner:{
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        }
+    }
+    ,{timestamps: true});
+
+videoSchema.plugin(mongooseAggregatePaginate);// Add pagination support
+
+export const Video = mongoose.model("Video", videoSchema);
