623fa1d1117937bb3eb3f12fea9b3dd639fff484
SourinChattaraj <sourinchattaraj99@gmail.com>
2025-05-27 14:20:58 +0530

    refactor: improve formatting and readability of asyncHandler function

--- src/utils/asynHandler.js
+++ src/utils/asynHandler.js
@@ -1,10 +1,11 @@
 // using promise to handle async errors in express
-const asyncHandler=(requestHandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next))
-        .catch((error)=>next(error));
-    }
-}
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise
+      .resolve(requestHandler(req, res, next))
+      .catch((error) => next(error));
+  };
+};
 export default asyncHandler;
 
 // using try-catch
