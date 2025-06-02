bafe66933e68333acf548a550feb10bff742bbc5
SourinChattaraj <sourinchattaraj99@gmail.com>
2025-06-03 02:13:05 +0530

    Fix route naming for refresh token endpoint by removing hyphen

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -21,6 +21,6 @@ router.route("/login").post(loginUser)
 // secure routes
 router.route("/logout").post(verifyJWT, logoutUser)
 
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refreshtoken").post(refreshAccessToken)
 
 export default router;
